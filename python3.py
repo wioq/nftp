74d38c965cd191a9f38631b71771c75d6045f1c8
Nadeem Ghafoor <nadeemghafoor650@gmail.com>
2021-11-03 00:18:20 +0500

    work done

---
+++ python3.py
@@ -0,0 +1,8 @@
+chr = input("Enter your Character:")
+if(chr >= 'A' and chr <='Z'):
+    print("You Enter chr as upper case Alphabat")
+elif(chr >= 'a' and chr <='z' ):
+  print("You Enter chr as lower case Alphabat")
+elif(chr >= '0' and chr <= '9'):
+  print("You Enter chr as an Integer")
+else:print("You Enter chr as a Special Symbole")
